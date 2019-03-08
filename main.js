534c79d631a027c534b16807adb46b6e9ed1fb68
Jason Park <jason.j.park79@gmail.com>
2019-03-08 10:26:54 -0500

    1

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-var PostModel = Backbone.Model.extend({
+var Post = Backbone.Model.extend({
   defaults: {
-    user: 'Aaron',
-    text: 'Hello!'
+    text: 'N/A',
+    points: 0,
+    isAwesome: false
+  },
+
+  initialize: function() {
+    // When `points` changes, update it!
+    this.on('change:points', this._updateIsAwesome);
+
+    // If we got a specific initial `points` value, ensure it's set correctly
+    // from the very beginning of this model's lifespan.
+    this._updateIsAwesome();
+  },
+
+  _updateIsAwesome: function() {
+    var isAwesome = this.get('points') >= 25;
+    this.set('isAwesome', isAwesome);
+  },
+
+  highFive: function() {
+    // When we change `points` here, an event is fired, triggering
+    // `#_updateIsAwesome`! Note that we don't have to worry about updating
+    // `isAwesome` directly during this method, since it gets taken care of by
+    // our event listener, set in `#initialize`.
+    if (this.points <= 45) {
+    this.set('points', this.get('points') + 5);
+  };
   }
 });
 
-var pm1 = new PostModel();
+var p = new Post();
+console.log(p.get('points')); // 0
 
-console.log(pm1);
-console.log(pm1.attributes);
+p.highFive();
+console.log(p.get('points')); // 5
+
+p.highFive();
+console.log(p.get('points')); // 10
+p.set('points', 30);
+var p2 = new Post({ points: 50 });
