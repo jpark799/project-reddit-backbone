8c656d6a70eb43c077712e43c3c451b659a96fb4
Jason Park <jason.j.park79@gmail.com>
2019-04-28 14:02:30 -0400

    finished reddit built with backbone.js

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
-var Post = Backbone.Model.extend({
-  defaults: {
-    text: 'N/A',
-    points: 0,
-    isAwesome: false
-  },
-
-  initialize: function() {
-    // When `points` changes, update it!
-    this.on('change:points', this._updateIsAwesome);
-
-    // If we got a specific initial `points` value, ensure it's set correctly
-    // from the very beginning of this model's lifespan.
-    this._updateIsAwesome();
-  },
-
-  _updateIsAwesome: function() {
-    var isAwesome = this.get('points') >= 25;
-    this.set('isAwesome', isAwesome);
-  },
-
-  highFive: function() {
-    // When we change `points` here, an event is fired, triggering
-    // `#_updateIsAwesome`! Note that we don't have to worry about updating
-    // `isAwesome` directly during this method, since it gets taken care of by
-    // our event listener, set in `#initialize`.
-    if (this.points <= 45) {
-    this.set('points', this.get('points') + 5);
-  };
+var PostModel = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      user: '',
+      post: ''
+    }
   }
 });
 
-var p = new Post();
-console.log(p.get('points')); // 0
+var PostView = Backbone.View.extend({
+  template:  Handlebars.compile($('#post-template').html()),
 
-p.highFive();
-console.log(p.get('points')); // 5
+  className: 'post',
 
-p.highFive();
-console.log(p.get('points')); // 10
-p.set('points', 30);
-var p2 = new Post({ points: 50 });
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+
+    return this;
+  }
+});
+
+
+$('.add-post').on('click', function () {
+  var text = $('#post-name').val();
+  var user = $('#user-name').val();
+
+  var postModel = new PostModel({ text: text, user: user});
+  var postView = new PostView({ model: postModel });
+
+  $('.posts').append(postView.render().el)
+});
+
